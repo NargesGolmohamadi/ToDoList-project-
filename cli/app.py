69a0e7b7be7f99997a6e8801f3a763d7bbe45fba
NargesGolmohamadi <nargesgol01@gmail.com>
2025-10-17 21:49:40 +0330

    complete cli

--- cli/app.py
+++ cli/app.py
@@ -25,4 +25,98 @@ def create_project():
     except ValueError as e:
         print(f"Error: {e}")
 
+def show_projects():
 
+    projects = project_service.list_projects()
+    if not projects:
+        print(" No projects found.")
+    else:
+        for p in projects:
+            print(f" {p.id}. {p.name} — {p.description} ({len(p.tasks)} tasks)")
+
+
+def add_task():
+
+    show_projects()
+    try:
+        pid = int(input("Enter project ID: "))
+        project = project_service.get_project_by_id(pid)
+        if not project:
+            print("Project not found.")
+            return
+
+        title = input("Task title: ")
+        desc = input("Task description: ")
+        task = task_service.add_task(project, title, desc)
+        print(f" Task added: {task}")
+    except ValueError as e:
+        print(f" Error: {e}")
+
+
+def show_tasks():
+    show_projects()
+    try:
+        pid = int(input("Enter project ID: "))
+        project = project_service.get_project_by_id(pid)
+        if not project:
+            print(" Project not found.")
+            return
+        if not project.tasks:
+            print(" No tasks for this project.")
+        else:
+            for t in project.tasks:
+                print(f"  {t.id}. {t.title} — status: {t.status}")
+    except ValueError as e:
+        print(f"Error: {e}")
+
+
+def edit_project():
+    show_projects()
+    try:
+        pid = int(input("Enter project ID to edit: "))
+        new_name = input("New project name: ")
+        new_desc = input("New project description: ")
+        updated = project_service.edit_project(pid, new_name, new_desc)
+        print(f" Project updated: {updated}")
+    except ValueError as e:
+        print(f" Error: {e}")
+
+
+def delete_project():
+    show_projects()
+    try:
+        pid = int(input("Enter project ID to delete: "))
+        if project_service.delete_project(pid):
+            print(" Project deleted.")
+        else:
+            print(" Project not found.")
+    except ValueError as e:
+        print(f"Error: {e}")
+
+
+def main():
+    while True:
+        print_menu()
+        choice = input("\nSelect an option: ")
+
+        if choice == "1":
+            create_project()
+        elif choice == "2":
+            show_projects()
+        elif choice == "3":
+            add_task()
+        elif choice == "4":
+            show_tasks()
+        elif choice == "5":
+            edit_project()
+        elif choice == "6":
+            delete_project()
+        elif choice == "7":
+            print("Goodbye!")
+            break
+        else:
+            print("Not an option!")
+
+
+if name == "main":
+    main()
